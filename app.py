f13953ccfa2d8abb20d3eb8d813588f8668a7a70
Sai Sampath Ayalasomayajula <arg5506@gmail.com>
2025-06-28 19:51:54 +0530

    Final release as of 28/06

--- app.py
+++ app.py
@@ -239,13 +239,12 @@ elif selected_job:
 st.subheader("✍️ Enhance My Resume")
 st.info("🚧 This feature is coming soon. Stay tuned for updates!")
 
-"""
-if uploaded_file:
-    if jd_source_text:
-        st.subheader("✍️ Enhance My Resume")
-        if st.button("Suggest Resume Lines"):
-            with st.spinner("Crafting smart resume lines..."):
-                enhancement_prompt = f"""
+#if uploaded_file:
+ #   if jd_source_text:
+  #      st.subheader("✍️ Enhance My Resume")
+   #     if st.button("Suggest Resume Lines"):
+    #        with st.spinner("Crafting smart resume lines..."):
+     #           enhancement_prompt = f"""
 #You are a helpful resume assistant. Given the following job description, suggest 3 impactful bullet points that I can add to my resume to match this role better.
 
 #Make them achievement-driven, specific, and skill-oriented.
